44b239562c387905cb92fd1ff5d6315db1f45dd1
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2022-07-18 22:37:09 +1000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. catchy marketing slogan here. 
+  Welcome to ${PREFIX}'s app. catchy marketing slogan here[attempt 2]. 
   <!-- END -->
   
   </div>
